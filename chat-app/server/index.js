56601a03ca07906c95335f228beabfbe83a4342e
Omnamahshivaa <160417172+Omnamahshivaa@users.noreply.github.com>
2025-07-25 13:41:04 +0530

    final

--- chat-app/server/index.js
+++ chat-app/server/index.js
@@ -36,6 +36,9 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
 // API Routes
+api.get('/',(req,res)=>{
+  res.send("Api is running")
+})
 app.use('/api/auth', authRoutes);
 app.use('/api/user', protectedRoutes);
 app.put('/api/messages/:id', verifyToken, async (req, res) => {
